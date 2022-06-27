70c2773d25ed4ca9ef0357628b9ef100f1bc7794
LeroyTechnologies <108254625+LeroyTechnologies@users.noreply.github.com>
2022-06-26 23:22:55 -0700

    Added MassEntityProcessorsAndFragmentsToCsv.ps1

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# UnrealEngineScripts
-Collection of scripts to help working with Unreal Engine
+# Unreal Engine Scripts
+Collection of scripts to help working with Unreal Engine.
+
+# MassEntityProcessorsAndFragmentsToCsv.ps1
+
+This script is useful for large UE5 projects that use the [MassEntity framework](https://docs.unrealengine.com/5.0/en-US/mass-entity-in-unreal-engine/), such as [City Sample](https://www.unrealengine.com/marketplace/en-US/product/city-sample). It produces a CSV which lists all processors and fragments used in the project, and their access modifiers.
+
+## Example Usage
+Example usage to copy CSV output to clipboard (make sure to use a valid path to .sln file):
+```
+MassEntityProcessorsAndFragmentsToCsv.ps1 -slnPath D:\UE5\CitySample\CitySample.sln | Set-Clipboard
+```
+
+## Example Output
+
+CSV in Google Sheets: https://docs.google.com/spreadsheets/d/1cV5LdfMd-Jj0PdczKeZLYpwrhCdmPPfa8gGXU_T9oQE/edit?usp=sharing
+
+## Script Tweaks
+
+There are several hard-coded parameters that can be tweaked at the bottom of the script:
+- cppFileRegex: A regex for which .cpp files to search for Mass Processor definitions
+- cppFileRegexToIgnore: A regex for files to ignore
+- processorNameRegexToIgnore: A regex for Processor class names to ignore
+
+Ideally these will be optional script parameters someday.
+
+## Access Modifier Abbreviations
+
+In the CSV the access modifiers are abbreviated as:
+- R: read access
+- W: write access
+- N: no access
+
+## Limitations
+
+Currently the script has the following limitations:
+- Does not have great error handling
+- Relies on regex parsing of C++ code, which can be brittle
+- Things like AddTagRequirement (tag requirements) or AddConstSharedRequirement (shared requirements) are not captured in the outputted CSV
+
+If anyone wants to submit a PR for any of these I'll more than happily accept it.
---
+++ scripts/MassEntityProcessorsAndFragmentsToCsv.ps1
@@ -0,0 +1,171 @@
+﻿param ([string] $slnPath)
+
+$global:fragmentIndices = @{}
+$global:lastFragmentIndex = 0
+$global:accessTypesToShortNames = @{
+    "EMassFragmentAccess::ReadWrite" = "RW"
+    "EMassFragmentAccess::ReadOnly" = "R"
+    "EMassFragmentPresence::Optional" = ""
+    "EMassFragmentAccess::None" = "N"
+    "EMassFragmentPresence::All" = ""
+    "EMassFragmentPresence::None" = ""
+}
+
+function GetCompiledFiles([string]$VcxProjPath, [string]$Regex, [string]$RegexToIgnore)
+{
+    $workingDir = Split-Path -Path $VcxProjPath -Parent
+    $xmldoc = New-Object System.Xml.XmlDocument
+    $xmlString = Get-Content $VcxProjPath -Raw
+    $xmldoc.LoadXml($xmlString)
+
+    # Create an XmlNamespaceManager for resolving namespaces
+    $nsmgr = New-Object System.Xml.XmlNamespaceManager($xmldoc.NameTable)
+    $nsmgr.AddNamespace("vcx", "http://schemas.microsoft.com/developer/msbuild/2003")
+
+    $root = $xmldoc.DocumentElement
+    $nodes = $root.SelectNodes("//vcx:ClCompile", $nsmgr)
+
+    $filteredFiles = @()
+    foreach ($node in $nodes)
+    {
+        $relativePath = $node.Attributes["Include"].Value
+        if(-not ($relativePath -match $Regex)) {
+            continue
+        }
+        if ($relativePath -match $RegexToIgnore) {
+            continue
+        }
+        $absPath = $relativePath
+        if ($absPath -match "\.\.") {
+            $absPath = Join-Path -Path $workingDir -ChildPath $relativePath -Resolve
+        }
+        $filteredFiles += $absPath
+    }
+    return ,$filteredFiles
+}
+
+function StringWithoutDuplicateChars([string]$string)
+{
+    $uniqueChars = $string.ToCharArray() | Sort-Object | Get-Unique
+    $result = $uniqueChars -join ""
+    return $result
+}
+
+function GetProcessorsForCppFile([string]$FilePath, [string]$processorNameRegexToIgnore)
+{
+    $processorsToFragments = @{}
+
+    $configureQueriesMatches = Get-Content $FilePath -Raw | Select-String '(?smi) (\w+?)::ConfigureQueries.+?\}' -AllMatches |%{$_.Matches}
+
+    foreach ($configureQueriesMatch in $configureQueriesMatches)
+    {
+        $processorName = $configureQueriesMatch.Groups[1].Value
+
+        if ($processorName -match $processorNameRegexToIgnore) {
+            continue
+        }
+
+        $processorFragments = @{}
+        $processorsToFragments.Add($processorName, $processorFragments)
+        $requirements = $configureQueriesMatch.Value | Select-String 'AddRequirement<(.+)>\((.+)\)' -AllMatches|%{$_.Matches}
+        foreach ($req in $requirements)
+        {
+            $fragmentName = $req.Groups[1].Value
+            if (!$global:fragmentIndices.Contains($fragmentName))
+            {
+                $global:fragmentIndices.Add($fragmentName, $global:lastFragmentIndex)
+                $global:lastFragmentIndex += 1
+            }
+
+            $accesses = $req.Groups[2].Value -split ","
+            $accessesShort = ""
+            foreach($accessUntrimmed in $accesses)
+            {
+                $access = $accessUntrimmed.Trim()
+                $accessShort = $global:accessTypesToShortNames[$access]
+                if ($accessShort -eq $null)
+                {
+                    $error = "Invalid access type: " + $access + ", in file: " + $FilePath
+                    throw $error
+                }
+                $accessesShort = StringWithoutDuplicateChars($accessesShort + $accessShort)
+            }
+
+            if ($processorFragments.Contains($fragmentName)) {
+                $newAccessesShort = $processorFragments[$fragmentName] + $accessesShort
+                $processorFragments[$fragmentName] = StringWithoutDuplicateChars($newAccessesShort)
+            } else {
+                $processorFragments.Add($fragmentName, $accessesShort)
+            }
+        }
+
+    }
+    return $processorsToFragments
+}
+
+function GetVcxProjectsFromSln([string]$slnPath)
+{
+    $slnDir = Split-Path -Path $slnPath -Parent
+    return Select-String -Path $slnPath -Pattern ', "(.+?\.vcxproj)' -AllMatches | %{$_.Matches} | %{$_.Groups[1]} | %{$_.Value} | %{Join-Path -Path $slnDir -ChildPath $_ -Resolve}
+}
+
+function GetProcessorsForSln([string]$slnPath, [string]$cppFileRegex, [string]$cppFileRegexToIgnore, [string]$processorNameRegexToIgnore)
+{
+    $processors = @()
+    $vcxProjects = GetVcxProjectsFromSln $slnPath
+    foreach($vcxProject in $vcxProjects)
+    {
+        $files = GetCompiledFiles $vcxProject $cppFileRegex $cppFileRegexToIgnore
+        foreach($file in $files)
+        {
+            $processors += GetProcessorsForCppFile $file $processorNameRegexToIgnore
+        }
+    }
+    return $processors
+}
+
+function ConvertProcessorsToCsv($processors)
+{
+    $fragmentNames = [string[]]::new($global:fragmentIndices.Count)
+    foreach ($fragmentNameAndIndex in $global:fragmentIndices.GetEnumerator())
+    {
+        $fragmentName = $fragmentNameAndIndex.Name
+        $fragmentIndex = $fragmentNameAndIndex.Value
+        $fragmentNames[$fragmentIndex] = $fragmentName
+    }
+    $fragmentNamesLine = $fragmentNames -join ","
+    $fragmentNamesLine = "Processor," + $fragmentNamesLine
+    write-output $fragmentNamesLine
+
+    foreach ($processorsInFile in $processors)
+    {
+        foreach ($processorNameAndFragments in $processorsInFile.GetEnumerator())
+        {
+            $row = [string[]]::new($global:fragmentIndices.Count+1)
+            $processorName = $processorNameAndFragments.Name
+            $row[0] = $processorName
+            $fragmentNamesToAccess = $processorNameAndFragments.Value
+            foreach ($fragmentNameAndIndex in $global:fragmentIndices.GetEnumerator())
+            {
+                $fragmentName = $fragmentNameAndIndex.Name
+                $fragmentIndex = $fragmentNameAndIndex.Value
+                $access = $fragmentNamesToAccess[$fragmentName]
+                $row[$fragmentIndex+1] = $access
+            }
+
+            $rowString = $row -join ","
+            write-output $rowString
+        }
+    }
+}
+
+function GetProcessorsCsvFromSln([string]$slnPath, [string]$cppFileRegex, [string]$cppFileRegexToIgnore, [string]$processorNameRegexToIgnore)
+{
+    $allProcessors = GetProcessorsForSln $slnPath $cppFileRegex $cppFileRegexToIgnore $processorNameRegexToIgnore
+    return ConvertProcessorsToCsv $allProcessors
+}
+
+$cppFileRegex = "Processors?\.cpp|Translator\.cpp"
+$cppFileRegexToIgnore = "(Houdini|Traffic|Debug|Initializer|Destructor|SmartObject)"
+$processorNameRegexToIgnore = "(Initializer|Destructor|UMassMoveToTargetProcessor|Debug)"
+GetProcessorsCsvFromSln $slnPath $cppFileRegex $cppFileRegexToIgnore $processorNameRegexToIgnore
